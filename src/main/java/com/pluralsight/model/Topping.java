62effd0a70cb06bb7d41cf0bfde7757396e0135b
ezrateshale <ezra.teshale12@gmail.com>
2026-05-28 17:18:16 -0500

    feat: ConsoleColor class Setup

--- src/main/java/com/pluralsight/model/Topping.java
+++ src/main/java/com/pluralsight/model/Topping.java
@@ -6,5 +6,4 @@ public record Topping(String name, double price, boolean isPremium) {
     public Topping(String name, boolean isPremium) {
         this(name, 0.0, isPremium);
     }
-
 }
---
+++ src/main/java/com/pluralsight/ui/util/ConsoleColor.java
@@ -0,0 +1,17 @@
+package com.pluralsight.ui.util;
+/**
+ * Utility holder for terminal text coloring matching the project style guide.
+ */
+public class ConsoleColor {
+    public static final String RESET = "\u001B[0m";
+
+    // Text Colors
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String PURPLE = "\u001B[35m";
+
+    // Text Styles
+    public static final String BOLD = "\u001B[1m";
+}
